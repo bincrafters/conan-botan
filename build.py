e4750bd06a7422efc80927534e9bd81a842e1ed2
Felix Morgner <felix.morgner@gmail.com>
2017-11-18 13:41:16 +0100

    fix python style

--- build.py
+++ build.py
@@ -1,5 +1,8 @@
+import os
+import re
+import platform
+
 from conan.packager import ConanMultiPackager
-import os, re, platform
 
 
 def get_value_from_recipe(search_string):
@@ -8,11 +11,14 @@ def get_value_from_recipe(search_string):
         result = re.search(search_string, contents)
     return result
 
+
 def get_name_from_recipe():
     return get_value_from_recipe(r'''name\s*=\s*["'](\S*)["']''').groups()[0]
 
+
 def get_version_from_recipe():
     return get_value_from_recipe(r'''version\s*=\s*["'](\S*)["']''').groups()[0]
+
 
 def get_default_vars():
     username = os.getenv("CONAN_USERNAME", "bincrafters")
@@ -20,26 +26,31 @@ def get_default_vars():
     version = get_version_from_recipe()
     return username, channel, version
 
+
 def is_ci_running():
-    return os.getenv("APPVEYOR_REPO_NAME","") or os.getenv("TRAVIS_REPO_SLUG","")
+    return os.getenv("APPVEYOR_REPO_NAME", "") or os.getenv("TRAVIS_REPO_SLUG", "")
+
 
 def get_ci_vars():
-    reponame_a = os.getenv("APPVEYOR_REPO_NAME","")
-    repobranch_a = os.getenv("APPVEYOR_REPO_BRANCH","")
+    reponame_a = os.getenv("APPVEYOR_REPO_NAME", "")
+    repobranch_a = os.getenv("APPVEYOR_REPO_BRANCH", "")
 
-    reponame_t = os.getenv("TRAVIS_REPO_SLUG","")
-    repobranch_t = os.getenv("TRAVIS_BRANCH","")
+    reponame_t = os.getenv("TRAVIS_REPO_SLUG", "")
+    repobranch_t = os.getenv("TRAVIS_BRANCH", "")
 
     username, _ = reponame_a.split("/") if reponame_a else reponame_t.split("/")
     channel, version = repobranch_a.split("/") if repobranch_a else repobranch_t.split("/")
     return username, channel, version
 
+
 def get_env_vars():
     return get_ci_vars() if is_ci_running() else get_default_vars()
 
+
 def get_os():
     return platform.system().replace("Darwin", "Macos")
-    
+
+
 if __name__ == "__main__":
     name = get_name_from_recipe()
     username, channel, version = get_env_vars()
@@ -47,13 +58,13 @@ if __name__ == "__main__":
     upload = "https://api.bintray.com/conan/{0}/public-conan".format(username)
 
     builder = ConanMultiPackager(
-        username=username, 
-        channel=channel, 
-        reference=reference, 
+        username=username,
+        channel=channel,
+        reference=reference,
         upload=upload,
-        remotes=upload, #while redundant, this moves bincrafters remote to position 0
-        upload_only_when_stable=True, 
+        remotes=upload,  # while redundant, this moves bincrafters remote to position 0
+        upload_only_when_stable=True,
         stable_branch_pattern="stable/*")
-        
+
     builder.add_common_builds(shared_option_name=name + ":shared", pure_c=False)
     builder.run()
