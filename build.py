7eb33f35b029b20f117a7397521f5d6fc67d55f3
SSE4 <tomskside@gmail.com>
2018-03-03 15:40:15 +0700

    - filter libcxx

--- build.py
+++ build.py
@@ -1,12 +1,19 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 
 from bincrafters import build_template_default
 
 if __name__ == "__main__":
 
-    builder = build_template_default.get_builder()
+    builder = build_template_default.get_builder(pure_c=False)
+
+    filtered_builds = []
+    for settings, options, env_vars, build_requires, reference in builder.items:
+        if settings['compiler'] == 'gcc' or settings['compiler'] == 'clang':
+            if settings['compiler.libcxx'] == 'libstdc++11' or settings['compiler.libcxx'] == 'libc++':
+                filtered_builds.append([settings, options, env_vars, build_requires])
+        else:
+            filtered_builds.append([settings, options, env_vars, build_requires])
+    builder.builds = filtered_builds
 
     builder.run()
-    
+
