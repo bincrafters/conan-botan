451bb5edc4f0a70218d1a5d7c2c11527f9e73068
Uilian Ries <uilianries@gmail.com>
2019-01-22 12:12:37 -0200

    fPIC should not be checked on Windows
    - Skip fPIC validation on Windows platform.

--- conanfile.py
+++ conanfile.py
@@ -137,7 +137,7 @@ class BotanConan(ConanFile):
 
         build_flags = []
 
-        if self.options.fPIC:
+        if self.settigngs.os != "Windows" and self.options.fPIC:
             build_flags.append('--cxxflags=-fPIC')
 
         if self.options.amalgamation:
