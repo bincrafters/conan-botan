e39a249face8da48acf8aca3bf323dd403c80867
René Meusel <rene.meusel@nexenio.com>
2019-02-07 09:34:01 +0100

    introduce a variable 'botan_extra_cxx_flags

--- conanfile.py
+++ conanfile.py
@@ -123,6 +123,7 @@ class BotanConan(ConanFile):
             botan_compiler = 'msvc'
 
         botan_abi_flags = []
+        botan_extra_cxx_flags = []
 
         if self._is_linux_clang_libcxx:
             botan_abi_flags.extend(["-stdlib=libc++", "-lc++abi"])
@@ -134,7 +135,10 @@ class BotanConan(ConanFile):
                 botan_abi_flags.append('-m64')
 
         if self.settings.os != "Windows" and self.options.fPIC:
-            botan_abi_flags.append('-fPIC')
+            botan_extra_cxx_flags.append('-fPIC')
+
+        if botan_extra_cxx_flags:
+            botan_abi_flags.extend(botan_extra_cxx_flags)
 
         botan_abi = ' '.join(botan_abi_flags) if botan_abi_flags else ' '
 
